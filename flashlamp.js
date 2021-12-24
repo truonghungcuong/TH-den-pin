6dee00c7057fe0ae8669f63ddbe5199353a41881
truonghungcuong <truonghungcuong12012001@gmail.com>
2021-12-24 14:14:16 +0700

    den pin

---
+++ flashlamp.js
@@ -0,0 +1,26 @@
+let FlashLamp = function () {
+
+    this.setBattery = function (battery) {
+        this.battery = battery;
+    };
+
+    this.getBatteryInfo = function () {
+        return this.battery.getEnergy();
+    };
+
+    this.light = function () {
+        if(this.status){
+            alert("Lighting");
+        } else {
+            alert("Not lighting");
+        }
+    };
+
+    this.turnOn = function () {
+        this.status = true;
+    };
+
+    this.turnOff = function () {
+        this.status = false;
+    }
+};
